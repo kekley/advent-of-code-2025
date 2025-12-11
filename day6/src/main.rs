b60086449119e21cd34e720d8cb7038a83bfd3dc
kekley <jesusmacias8568@gmail.com>
2025-12-10 23:22:44 -0800

    day 6 done

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,7 +2,12 @@ pub fn main() {
     let problems = parse_input(INPUT);
     let sum = problems.solve();
 
-    println!("Sum:{sum}");
+    println!("Sum: {sum}");
+
+    let problems2 = parse_input2(INPUT);
+    let sum = problems2.solve();
+
+    println!("Sum2: {sum}");
 }
 
 const INPUT: &str = include_str!("../input.txt");
@@ -10,6 +15,24 @@ const INPUT: &str = include_str!("../input.txt");
 struct Problems {
     numbers: [Vec<u64>; 4],
     operations: Vec<Operation>,
+}
+
+struct Problems2 {
+    numbers: Vec<Vec<u64>>,
+    operations: Vec<Operation>,
+}
+
+impl Problems2 {
+    pub fn solve(&self) -> u64 {
+        self.numbers
+            .iter()
+            .zip(self.operations.iter())
+            .map(|(nums, op)| match op {
+                Operation::Add => nums.iter().sum::<u64>(),
+                Operation::Mul => nums.iter().product::<u64>(),
+            })
+            .sum()
+    }
 }
 
 impl Problems {
@@ -40,41 +63,57 @@ enum Operation {
     Add,
     Mul,
 }
-fn parse_input2(input: &str) -> Problems {
+fn parse_input2(input: &str) -> Problems2 {
     let mut lines = input.trim_end().lines();
-    let nums_1 = lines.next().unwrap().split_ascii_whitespace();
-    let nums_2 = lines.next().unwrap().split_ascii_whitespace();
-    let nums_3 = lines.next().unwrap().split_ascii_whitespace();
-    let nums_4 = lines.next().unwrap().split_ascii_whitespace();
+    let nums_1 = lines.next().unwrap().as_bytes();
+    let nums_2 = lines.next().unwrap().as_bytes();
+    let nums_3 = lines.next().unwrap().as_bytes();
+    let nums_4 = lines.next().unwrap().as_bytes();
     let ops = lines.next().unwrap().split_ascii_whitespace();
+    let stride = nums_1.len();
 
-    while let (Some(n1), Some(n2), Some(n3), Some(n4)) =
-        (nums_1.next(), nums_2.next(), nums_3.next(), nums_4.next())
-    {
-        let chars1 = n1.as_bytes().iter().rev().collect::<Vec<_>>();
-
-        let chars2 = n2.as_bytes().iter().rev().collect::<Vec<_>>();
-
-        let chars3 = n3.as_bytes().iter().rev().collect::<Vec<_>>();
-
-        let chars4 = n4.as_bytes().iter().rev().collect::<Vec<_>>();
-    }
-
-    let ops = ops
-        .map(|op| {
-            if op == "+" {
-                Operation::Add
-            } else {
-                Operation::Mul
+    let mut numbers: Vec<Vec<u64>> = vec![];
+    let operations: Vec<Operation> = ops
+        .map(|op| match op {
+            "+" => Operation::Add,
+            "*" => Operation::Mul,
+            _ => {
+                unreachable!()
             }
         })
         .collect();
 
-    let numbers = [nums_1, nums_2, nums_3, nums_4];
+    let mut inner_vec = vec![];
 
-    Problems {
+    let slices = [nums_1, nums_2, nums_3, nums_4];
+
+    for i in 0..stride {
+        let mut num: u64 = 0;
+        for slice in slices {
+            let byte = slice[i];
+            if byte != b' ' {
+                num *= 10;
+                num += (byte - b'0') as u64;
+            }
+        }
+        if num == 0 && !inner_vec.is_empty() {
+            numbers.push(inner_vec.clone());
+            inner_vec.clear();
+        } else if num != 0 {
+            inner_vec.push(num);
+        } else {
+            unreachable!();
+        }
+    }
+    numbers.push(inner_vec);
+
+    dbg!(operations.len());
+    dbg!(numbers.len());
+
+    assert!(operations.len() == numbers.len());
+    Problems2 {
         numbers,
-        operations: ops,
+        operations,
     }
 }
 
